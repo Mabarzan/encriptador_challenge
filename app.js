430cdd50e121543cf9e086690b4ae16ce6a532c8
Mabarzan <abarza.manuel2@gmail.com>
2024-07-16 00:15:09 -0400

    se agrega funcionalidad al btn encriptar

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const t_area = document.getElementById('container_txt');
 const btn_encript = document.getElementById('btn_encript');
+const btn_desencript = document.getElementById('btn_desencript');
+const img_muneco = document.getElementById('muneco');
+const mensajeFinal = document.getElementById('container_mensajefin');
+const mensaje_find = document.getElementById('txt_mensaje_find');
+const derecha = document.getElementById('container_txt_desencript');
 //ajustar la altura del textarea automaticamente
 t_area.addEventListener('input', function(){
     this.style.height = 'auto';
@@ -8,21 +13,45 @@ t_area.addEventListener('input', function(){
 t_area.style.height = '36px';
 
 let cambio = [
-    {letra: 'a', reemplazo: 'ai'},
-    {letra: 'e', reemplazo:'enter'},
-    {letra:'i', reemplazo:'imes'},
-    {letra:'o', reemplazo: 'ober'},
-    {letra:'u', reemplazo: 'ufat'}
+    ["e", "enter"],
+    ["o", "ober"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["u", "ufat"],
 ]
 btn_encript.addEventListener("click", () => 
     {
-        let mensajOriginal = t_area.value.toLowerCase();
-        let mensajEncriptado= mensajOriginal.replace(/a|e|i|o|u/g, function(match){
-            let reemplazo = cambio.find(item => item.letra === match);
-            return reemplazo ? reemplazo.reemplazo : match;
-
-    });
-    t_area.value = mensajEncriptado;
-    console.log(mensajOriginal);
-    console.log(mensajEncriptado);
+    const textiano = t_area.value.toLowerCase();
+    function encriptar(nvotxxto)
+    {
+        for(let i=0; i < cambio.length; i++)
+            {
+                if(nvotxxto.includes(cambio[i][0]))
+                    {
+                        nvotxxto = nvotxxto.replaceAll(cambio[i][0], cambio[i][1]);
+                    };
+            };
+        return nvotxxto;
+    }
+    const textoEncrip = encriptar(textiano);
+    img_muneco.style.display = 'none';
+    mensaje_find.style.display = 'none';
+    derecha.classList.add("ajustar");
+    mensajeFinal.innerHTML = textoEncrip;
+}); 
+btn_desencript.addEventListener("click", () => 
+    {
+    const textiano = t_area.value.toLowerCase();
+    function desenencriptar(nvotxxto)
+    {
+        for(let i=0; i < cambio.length; i++)
+            {
+                if(nvotxxto.includes(cambio[i][1]))
+                    {
+                        nvotxxto = nvotxxto.replaceAll(cambio[i][1], cambio[i][0]);
+                    };
+            };
+        return nvotxxto;
+    }
+        console.log(desenencriptar(textiano));
 }); 
